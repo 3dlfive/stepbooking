40707a2bb1676d8c80107ee7d6d639c442581913
3lfive <denystry@gmail.com>
2023-11-01 16:50:18 -0400

    changes: change Dao interface

--- src/main/java/BookingApp/CollectionBooking/CollectionBooking.java
+++ src/main/java/BookingApp/CollectionBooking/CollectionBooking.java
@@ -5,7 +5,7 @@ import BookingApp.Booking.Booking;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class CollectionBooking implements Serializable {
+public class CollectionBooking<B> implements Serializable {
     static final long serialVersionUID = 1;
 
     ArrayList<Booking> db = new ArrayList<>();
--- src/main/java/BookingApp/bookingDAO/BookingDao.java
+++ src/main/java/BookingApp/bookingDAO/BookingDao.java
@@ -1,8 +1,15 @@
 package BookingApp.bookingDAO;
 
 import BookingApp.Booking.Booking;
+import BookingApp.CollectionBooking.CollectionBooking;
+
+import java.util.ArrayList;
 
 public interface BookingDao {
 
+CollectionBooking<Booking> getAllBookings();
 Booking getByID(int index);
+Boolean deleate(Booking booking);
+Boolean save(Booking booking);
+ArrayList<Booking> loadData();
 }
