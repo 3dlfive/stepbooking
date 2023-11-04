937071b8d6a890e85ee6b6f991525a815b59e91d
Vladimir <send4vladimir@gmail.com@>
2023-11-04 13:57:24 +0200

    added enums

---
+++ src/main/java/Flights/Enums/Airport.java
@@ -0,0 +1,24 @@
+package Flights.Enums;
+
+public enum Airport {
+    Augsburg,
+    Berlin,
+    Dortmund,
+    Düsseldorf,
+    Munich,
+    Helsinki,
+    Malmi,
+    Vaasa,
+    Enontekiö,
+    Belley,
+    Cannes,
+    Marseille,
+    Heathrow,
+    Manchester,
+    Birmingham,
+    Southend,
+    Dallas,
+    Denver,
+    Chicago,
+    Phoenix,
+}
