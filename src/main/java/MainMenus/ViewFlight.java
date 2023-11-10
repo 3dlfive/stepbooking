8cc1fed8c09ce23c15e1a9ef14f69da93d423733
Vladimir <send4vladimir@gmail.com@>
2023-11-10 20:52:29 +0200

    added functional to ViewFlight

--- src/main/java/MainMenus/ViewFlight.java
+++ src/main/java/MainMenus/ViewFlight.java
@@ -1,11 +1,32 @@
 package MainMenus;
 
 
-public class ViewFlight implements Command {
+import Flights.CollectionFlightDAO.CollectionFlightDAO;
+import Flights.ControllerFlight.ControllerFlight;
+import Flights.FlightDAO.FlightDao;
+import Flights.ServiceFlight.ServiceFlight;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ViewFlight implements Command {
+    FlightDao dao = new CollectionFlightDAO(new ArrayList<>());
+    ServiceFlight sf = new ServiceFlight(dao);
+    ControllerFlight controllerFlight = new ControllerFlight(sf);
+    private static final Scanner in = new Scanner(System.in);
     @Override
     public void apply() {
-
+        try {
+            controllerFlight.loadFromFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found it generate it");
+            controllerFlight.generateFlights(500);
+            controllerFlight.saveToFile();
+        }
+        System.out.println("ID?");
+        int id = Integer.parseInt(in.nextLine());
+        System.out.println(controllerFlight.getByID(id).toString());
     }
 
     @Override
