af01555a1fa7c036afa4060a8d794d136a959164
Denys <denystry@gmail.com>
2023-11-10 13:12:42 -0500

    68 мої рейси (#69)
    * feat: menu My flights
    
    * fix:fix test suite add table package

--- src/main/java/MainMenus/ManageBooking/FindBooking/FindBookings.java
+++ src/main/java/MainMenus/ManageBooking/FindBooking/FindBookings.java
@@ -13,6 +13,7 @@ public class FindBookings implements Command {
         Menu menu3 = new Menu();
         menu3.add("Search by id.", new SByIdBookings());
         menu3.add("Search by Name Lastname ", new SByNandLnBookings());
+        menu3.add("Search by Name or Lastname ", new SByNorLnBookings());
         menu3.add("Back", new Exit());
 
 
--- src/main/java/MainMenus/MyFlights.java
+++ src/main/java/MainMenus/MyFlights.java
@@ -1,13 +1,26 @@
 package MainMenus;
 
 
-public class MyFlights implements Command {
+import BookingApp.Controller.BookingController;
 
+import java.util.Scanner;
+
+public class MyFlights implements Command {
+    BookingController controller = new BookingController();
+    private static final Scanner in = new Scanner(System.in);
     @Override
-    public void apply() {
+    public void apply()  {
+
+        controller.loadData();
+        System.out.println("Your name?");
+        String name = in.nextLine();
+        System.out.println("Your lastname?");
+        String lastname =in.nextLine();
+        controller.search(name,lastname);
+
+
 
     }
-
     @Override
     public boolean isExit() {
         return Command.super.isExit();
